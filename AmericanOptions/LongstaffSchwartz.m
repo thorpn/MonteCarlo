4af60ffc08d8999a5736266493d8f03005b04830
TPN <thorpn86@gmail.com>
2022-12-19 12:01:16 +0100

    Update LongstaffSchwartz.m

--- AmericanOptions/LongstaffSchwartz.m
+++ AmericanOptions/LongstaffSchwartz.m
@@ -4,17 +4,15 @@
 %   Comments: 
 %   0. The brute force method is way too slow: Monte Carlo in Monte Carlo to estimate
 %   continuation value
-%   1. Not at all optimized code
-%   2. note we truncate on x variable to only take paths in the money which has been shown to "improve performance" of
-%   method
+%   1. Not at all optimized, I am honestly embarassed about how bad this
+%   code is
+%   2. note we truncate on x variable to only take paths in the money which has been shown to improve performance of
+%   method. 
 %   3. We are using constant interest rate and fixed step size.
 %   4. We are estimating a stopping rule but we wont get the optimal one so
 %   we have a slight negative bias
-%   5. Note this is the simplest possible example; We could also have bonds which depend on many tenor points on interest rate curve
-%   6. We could also have an option like max(S1,S2,S3,0) - this would give rise to more complex regression steps including interaction terms etc.
-%   7. Current best practice is to use Chebyshev polynomials or Neural Networks instead of regression
-%   8. Original article;
-%   Longstaff-Schwartz: https://people.math.ethz.ch/~hjfurrer/teaching/LongstaffSchwartzAmericanOptionsLeastSquareMonteCarlo.pdf
+%
+%   Longstaff-Schwartz (LS): https://people.math.ethz.ch/~hjfurrer/teaching/LongstaffSchwartzAmericanOptionsLeastSquareMonteCarlo.pdf
 %
 %
 clear all; clc;
@@ -40,31 +38,33 @@ addpath(genpath('D:\Google Drive\Ekstern lektor\Monte carlo - Seminar\Youtube\20
 %Simulates GBM
 S = fGeometricBrownianMotion(T,n,r,sigma,S0,R)';
 
-%% Replicates tabels from LS
+%% Replicates tabelel of stock price paths from LS
 
 S = [1 1.09 1.08 1.34; 
-1 1.16 1.26 1.54;
-1 1.22 1.07 1.03;
-1 0.93 0.97 0.92;
-1 1.11 1.56 1.52;
-1 0.76 0.77 0.9;
-1 0.92 0.84 1.01;
-1 0.88 1.22 1.34];
+     1 1.16 1.26 1.54;
+     1 1.22 1.07 1.03;
+     1 0.93 0.97 0.92;
+     1 1.11 1.56 1.52;
+     1 0.76 0.77 0.9;
+     1 0.92 0.84 1.01;
+     1 0.88 1.22 1.34];
 
 %%
 
 %Gets discounts
-df=exp(-r*dt);
+%Note we are using a fixed interest rate
+%But we could also have simulated one or used a non constant one
+df=(exp(-r*dt).^(1:n))';
 
-%Gets final value
-val = zeros(R,n+1);
-val(:,end) = h(S(:,end),k);
+%Stopping time indicators
+ST = zeros(R,n);
+ST(:,end) = h(S(:,end),k)> 0;
 
-%Stoppingtime and cashflow matrices
+%cashflow matrices
 CF = zeros(R,n);
-CF(:,end) = val(:,end);
+CF(:,end) = h(S(:,end),k);
 
-%Off-line estimation
+%Loops over states starting at T-1
 for i = 1:n-1
     
     %State payoffs
@@ -73,13 +73,14 @@ for i = 1:n-1
     %(Note code only works for in the money from starts options)
     paths = payoffs>0; 
     
-    %Selects X and Y (discounted)
+    %Selects X
+    %pretty application specific, eg. multiple bonds or tenors for bonds
+    %etc.
     x = S(:,end-i);
     X = [ones(R,1) x x.^2];
     
     %For each column forward in CF, discount appropriately to current time
-    Y = (CF(:,(end-i+1):end))*(df.^(1:i))';
-    %Y = val(:,end-i+1).*df;
+    Y = (CF(:,(end-i+1):end))*df(1:i);
 
     %Estimates Beta on paths in the money
     beta = mvregress(X(paths,:),Y(paths,:));
@@ -89,19 +90,36 @@ for i = 1:n-1
     
     %Longstaff & Schwartz approach
     Indicator = payoffs > ContVal;
-    val(paths,end-i) = Indicator(paths).*payoffs(paths)+(1-Indicator(paths)).*ContVal(paths);
 
-    %Stopping times
-    
+    %Stopping time indicator updating
+    %Horribly inefficient (uses for loops and runs through all columsn
+    %every time!)
+    ST(paths,end-i) = Indicator(paths);
+    %Loops rows
+    for j=1:R
+        %reset called indicator
+        called=0;
+        %Loops columns
+        for l=1:n
+            %If called already we cant call it later
+            if and(ST(j,l)==1,called==1) 
+                called=1;
+                ST(j,l)=0;
+            end
+            
+            %Indicator for option having been called first time
+            if and(ST(j,l)==1,called==0) 
+                called=1;
+            end
+        end
+    end
     
     %Current cash flow
     CF(:,end-i) = h(S(:,end-i),k).*Indicator;
     
-    %Update previous Cash flows
-    CF(:,end-i+1) = h(S(:,end-i+1),k).*(1-Indicator);
+    %Update previous Cash flows to avoid calling option in multiple periods
+    CF = CF.*ST;
 end
 
-%Estimates t=0 value, seems slightly off ?
-%4.472
-mean(val(:,1))
-
+%Price matches Longstaff-Schwartz
+mean(CF*df)
